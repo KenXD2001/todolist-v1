36c243e3c24e69065472e68ec4db6fad48d08043
Ken <ashwinb708@gmail.com>
2023-08-29 21:08:18 +0530

    Update app.js and list.ejs

--- app.js
+++ app.js
@@ -6,42 +6,35 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = ["Breakfast", "Lunch", "Dinner"];
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
 
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+    
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+    };
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default:
-            day = "Unknown";
-            console.log("Error: current day is equal to: " + currentDay);
-    }
+    var day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", {kindOfDay: day});
+    res.render("list", {kindOfDay: day, newListItems: items});
+
+});
+
+app.post("/", function(req, res) {
+    var item = req.body.newItem;
+    console.log(item);
+
+    items.push(item);
+
+    res.redirect("/");
 
 });
 
